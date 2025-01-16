d7217c6528f714d5dc8ff70bb043df1f83c707ce
Maxime <maxime.ducasse@viacesi.fr>
2025-01-16 15:49:11 +0100

    Mise a jour de la state Machine

--- src/state_machine/src/StateMachine.py
+++ src/state_machine/src/StateMachine.py
@@ -4,30 +4,10 @@ import smach
 import smach_ros
 from std_srvs.srv import Empty
 from std_msgs.msg import String
-from StateScanTable import TableScanState
-from StateScanBook import BookScanState
-from StateListen import ListenState
-from ErrorState import ErrorState
 
-
-class StartState(smach.State):
-    def __init__(self):
-        smach.State.__init__(self, outcomes=['scanTable'])
-    
-    def execute(self, userdata):
-        rospy.loginfo("Etat de départ : Passage à l'état Scan.")
-        return 'scanTable'
-
-class MoveState(smach.State):
-    def __init__(self):
-        smach.State.__init__(self, outcomes=['idle'])
-        # Vous pouvez ajouter une logique ici pour gérer le mouvement.
-    
-    def execute(self, userdata):
-        rospy.loginfo("Etat Move : Exécution d'un mouvement ou d'une action.")
-        rospy.sleep(2)  # Simule une action ou un mouvement
-        rospy.loginfo("Etat Move : Mouvement terminé, retour à l'état Idle.")
-        return 'idle'  # Retour à l'état Idle après Move
+from states.IdleState import IdleState
+from states.ScanTableState import TableScanState
+from states.StartState import StartState
 
 def main():
     rospy.init_node("state_machine_example")
@@ -37,11 +17,8 @@ def main():
     
     with sm:
         smach.StateMachine.add('START', StartState(), transitions={'scanTable': 'SCANTABLE'})
-        smach.StateMachine.add('SCANTABLE', TableScanState(), transitions={'scanBook': 'SCANBOOK','scanTable': 'SCANTABLE', 'error':'ERROR'})
-        smach.StateMachine.add('SCANBOOK', BookScanState(), transitions={'listen': 'LISTEN','scanBook': 'SCANBOOK', 'error':'ERROR'})
-        smach.StateMachine.add('LISTEN', ListenState(), transitions={'move': 'MOVE'})
-        smach.StateMachine.add('MOVE', MoveState(), transitions={'listen': 'LISTEN'})
-        smach.StateMachine.add('ERROR', ErrorState(previous_state_name='UNKNOWN'), transitions={'exit': 'EXIT'})
+        smach.StateMachine.add('SCANTABLE', TableScanState(), transitions={'scanBook': 'IDLE', 'error':'IDLE'})
+        smach.StateMachine.add('IDLE', IdleState(), transitions={})
 
     # Activer le serveur d'introspection SMACH
     sis = smach_ros.IntrospectionServer('smach_server', sm, '/SM_ROOT')
---
+++ src/state_machine/src/states/ScanTableState.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import rospy
+import smach
+from table_detector.srv import TableDetector
+from utils.TTS import TextToSpeech
+
+class TableScanState(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['scanBook', 'error'])
+        self.service_name = 'table_detector'
+        self.TTS = TextToSpeech()
+
+    def execute(self, userdata):
+        rospy.loginfo("Etat Scan : Appel au service 'table_detector'.")
+        
+        # Attendre que le service soit disponible
+        rospy.wait_for_service(self.service_name)
+        
+        try:
+            # Créer un client de service et appeler le service
+            scan_table = rospy.ServiceProxy(self.service_name, TableDetector)
+            r = scan_table()
+            
+            if r.result == -1:
+                self.TTS.say("Table non détéctée. Veuillez vérifier mon environnement. Pour plus d'informations, référez-vous à la documentation d'utilisation.")
+            else:
+                self.TTS.say("Table détéctée. Environnement mis à jour !")
+                
+            rospy.loginfo("Etat Scan : Réponse du service reçue, passage à l'état Idle.")
+            return "scanBook"
+        except rospy.ServiceException as e:
+            rospy.logerr(f"Erreur lors de l'appel au service 'table_detector' : {e}")
+            return "error"

0d37082e392c9960d4d605f8147307d04d714437
Simon <simon.bello@viacesi.fr>
2025-01-17 13:08:04 +0100

    return error if flyer not found

--- src/state_machine/src/states/StateScanBook.py
+++ src/state_machine/src/states/StateScanBook.py
@@ -17,13 +17,11 @@ class BookScanState(smach.State):
             2: "2"
         }
 
-
     def execute(self, userdata):
         rospy.loginfo("Etat Scan : Appel au service 'book_detector'.")
         
         # Attendre que le service soit disponible
         rospy.wait_for_service(self.service_name)
-
 
         try:
             scan_book = rospy.ServiceProxy(self.service_name, FindAruco)
@@ -31,6 +29,10 @@ class BookScanState(smach.State):
                 # Créer un client de service et appeler le service
                 self.tts.say(f"Je lance la recherche du flyer {flyer + 1}")
                 r = scan_book(int(aruco_id))
+                if (r.coordinates.pose.position.x == 0 and r.coordinates.pose.position.y == 0 and r.coordinates.pose.position.z ==0):
+                    self.tts.say(f"je n'ai pas trouvé le flyer {flyer + 1}")
+                    rospy.sleep(5)
+                    return "error"
                 rospy.sleep(3)
                 rospy.loginfo("Etat Scan : Réponse du service reçue, passage à l'état Listen.")
             self.tts.say("Les 3 flyers sont bien présents dans mon environnement")

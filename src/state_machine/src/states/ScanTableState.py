087765a63f2d7465db66954f00de464d167b9be4
Simon <simon.bello@viacesi.fr>
2025-01-17 14:15:51 +0100

    Error previous state

--- src/state_machine/src/states/ScanTableState.py
+++ src/state_machine/src/states/ScanTableState.py
@@ -6,12 +6,13 @@ from utils.TTS import TextToSpeech
 
 class ScanTableState(smach.State):
     def __init__(self):
-        smach.State.__init__(self, outcomes=['scanBook', 'error'])
+        smach.State.__init__(self, outcomes=['scanBook', 'error'],input_keys=['sm_previous_state'],output_keys=['sm_previous_state'])
         self.service_name = 'table_detector'
         self.TTS = TextToSpeech()
 
     def execute(self, userdata):
         rospy.loginfo("Etat Scan : Appel au service 'table_detector'.")
+        userdata.sm_previous_state = 'Scan Table'
         
         # Attendre que le service soit disponible
         rospy.wait_for_service(self.service_name)
@@ -23,6 +24,8 @@ class ScanTableState(smach.State):
             
             if r.result == -1:
                 self.TTS.say("Table non détéctée. Veuillez vérifier mon environnement. Pour plus d'informations, référez-vous à la documentation d'utilisation.")
+                rospy.sleep(5)
+                return "error"
             else:
                 self.TTS.say("Table détéctée. Environnement mis à jour !")
                 
